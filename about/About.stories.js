80288dbac183dab1134e8a0d3d4b4de0e2d81548
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-08-18 18:45:08 -0400

    boilerplate

---
+++ about/About.stories.js
@@ -0,0 +1,15 @@
+import About from './About'
+
+export default {
+  title: 'Screens/About',
+  component: About
+}
+
+const Template = (args) => <About {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+
+}
+
+
